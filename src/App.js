78fbb36ed46d0fc82f99c9d32b99de713be82c7b
brendan sudol <brendansudol@gmail.com>
2018-02-15 23:01:51 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return <div className="p2">hello, world</div>
+  }
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+import App from './App'
+
+import 'ace-css/css/ace.min.css'
+import './index.css'
+
+render(<App />, document.getElementById('root'))
