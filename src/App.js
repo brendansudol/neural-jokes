c25f1c4b468b0d7e0e37cd9f6fe703518f8d6324
brendan sudol <brendansudol@gmail.com>
2018-02-17 23:17:23 -0500

    add temp

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ const modelPath = `${process.env.PUBLIC_URL}/static/models/lstm/hemingway`
 const lstm = new ml5.LSTMGenerator(modelPath)
 
 class App extends Component {
-  state = { loading: false, output: '', text: 'There was' }
+  state = { loading: false, output: '', temp: '0.5', text: 'There was' }
 
   shouldComponentUpdate(nextProps, nextState) {
     return true
   }
 
   handleChange = e => {
-    this.setState({ text: e.target.value })
+    const { name, value } = e.target
+    this.setState({ [name]: value })
   }
 
   handleSubmit = e => {
@@ -25,11 +26,11 @@ class App extends Component {
   initGen = () => setTimeout(this.runGen, 20)
 
   runGen = () => {
-    const { text } = this.state
+    const { temp, text } = this.state
     const data = {
       seed: text,
-      temperature: 0.5,
-      length: 100,
+      temperature: +temp,
+      length: 200
     }
 
     lstm.generate(data, this.processResults)
@@ -42,19 +43,35 @@ class App extends Component {
   }
 
   render() {
-    const { loading, output, text } = this.state
+    const { loading, output, temp, text } = this.state
 
     return (
       <div className="p2 sm-p3 container">
-        <form className="sm-flex mb3" onSubmit={this.handleSubmit}>
-          <input
-            className="flex-auto input sm-mb0 mr1"
-            name="text"
-            value={text}
-            onChange={this.handleChange}
-          />
-          <button type="submit" className="btn btn-primary">
-            Generate
+        <form className="mb3" onSubmit={this.handleSubmit}>
+          <div className="mb3">
+            <label>Seed</label>
+            <input
+              className="input m0"
+              name="text"
+              value={text}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="mb3">
+            <label>Temperature: ({parseInt(temp * 10, 10)} / 10)</label>
+            <input
+              type="range"
+              className="input-range block"
+              name="temp"
+              min="0"
+              max="1"
+              step="0.1"
+              value={temp}
+              onChange={this.handleChange}
+            />
+          </div>
+          <button type="submit" className="btn btn-primary" disabled={loading}>
+            {loading ? 'Generating' : 'Generate'}
           </button>
         </form>
         {loading && <div>Loading...</div>}
